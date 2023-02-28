60ddab28663b3836920810adec955573476eb4ed
Alejandro Grimaldo <alejandrogrim12@gmail.com>
2023-02-28 15:22:57 -0600

    hola

---
+++ binarysearch.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void solve()
+{
+    // Add your solution here
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int tc = 1;
+    // cin >> tc;
+    // aqui va el arreglo
+    long long arr[7] = {1, 2, 3, 4, 5, 6};
+
+    for (int t = 1; t <= tc; t++)
+    {
+        // cout << "Case #" << t << ": ";
+        solve();
+    }
+}
